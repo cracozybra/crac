4ba686ad6200bfee826e485c520ad496162886b3
cracozybra <vekmvekmrf@mail.ru>
2023-08-09 21:53:07 +0300

    knb

--- main.go
+++ main.go
@@ -1,118 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	/*imia := 10
+	memes()
 
-	fmt.Print("popka \n ianka \t kakaska", " \n axaxa \n")
-	if imia == 10 {
-		imia += 10
-	} else {
-		imia -= 10
-	}
-	fmt.Print("imia=", imia)
+}
+func meme() {
+	p := 0
+	c := 0
+	for (c != 3) && (p != 3) {
 
-	//(цикл) for if(условие)
-	for i := 0; i != 10; i++ {
-		if i%2 == 0 {
-			fmt.Print("\n", i)
-		}
+		fmt.Println("Введите: камень, ножницы или бумага")
+		a := ""
+		fmt.Scanf("%s\n", &a)
+		arr := [3]string{"бумага", "ножницы", "камень"}
+		x := arr[rand.Intn(len(arr))]
+		fmt.Println("Компьютер выбрал ", x)
 
+		if a == x {
+			fmt.Println("Ничья :(")
+		} else {
 
-	}
+			if (a == "камень" && x == "ножницы") || (a == "бумага" && x == "камень") || (a == "ножницы" && x == "бумага") {
 
-	//массивы()
-	var faf [16]int
+				p++
+			} else {
 
-	for a := 0; a < len(faf); a++ {
-		faf[a] = a
-	}
-	fmt.Println(faf)
-
-
-				for a := 0; a < len(faf); a++ {
-					if faf[a]%2 == 0 {
-						fmt.Print(faf[a], " ")
-					} else {
-						fmt.Print("* ")
-					}
-
-				}
-			c := 0
-			for a := 0; a < len(faf); a++ {
-				if faf[a]%2 == 1 {
-
-					c = c + faf[a]
-				}
+				c++
 			}
-			fmt.Print(c)
-
-		s := make(map[string]int)
-		s["Яна кака"] = 2
-		fmt.Println(s["Яна кака"]) */
-	for {
-
-		h := menu()
-		fmt.Println("Введите первое число")
-		a := 0
-		b := 0
-		fmt.Scanf("%d\n", &a)
-		fmt.Println("Введите второе число")
-		fmt.Scanf("%d\n", &b)
-
-		//fmt.Scanf("%d\n", &s)
-		switch h {
-		case 1:
-			fmt.Println(sum(a, b))
-		case 2:
-			fmt.Println(vich(a, b))
-		case 3:
-			fmt.Println(umn(a, b))
-		case 4:
-			fmt.Println(del(a, b))
-		case 5:
-			fmt.Println(voz(a, b))
-		case 6:
-			return
 
 		}
+		fmt.Println("Ваш счёт:", p, "\nСчёт компьютера: ", c)
+
 	}
-
-}
-func sum(a int, b int) int {
-
-	return a + b
-
-}
-func vich(a int, b int) int {
-
-	return a - b
-
-}
-func umn(a int, b int) int {
-
-	return a * b
-
-}
-func del(a int, b int) int {
-
-	return a / b
-
-}
-func voz(a int, b int) int {
-
-	if b == 1 {
-		return a
+	if p > c {
+		fmt.Println("Вы выйграли!!!")
 	} else {
-		return a * voz(a, b-1)
+		fmt.Println("Я выйграл :)")
 	}
-
 }
-func menu() int {
-	fmt.Println("Выберите пункт меню:\n1 - сложение\n2- вычитание\n3- умножение\n4-деление\n5-возведение в степень\n6-выход")
-	var s int
+func memes() {
 
-	fmt.Scanf("%d\n", &s)
-	return s
 }
